3cd3c62c4ab75f1a444e1099ef487a44934fdc22
Aarushi Agarwal <132547930+Aarushi4Agarwal@users.noreply.github.com>
2024-04-20 21:08:36 +0530

    committing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import {BsPerson} from 'react-icons/bs'
-import {BiSearch} from 'react-icons/bi'
-import {AiOutlineClose} from 'react-icons/ai'
-import {HiOutlineMenuAlt4} from 'react-icons/hi'
-import {FaFacebook, FaTwitter, FaInstagram, FaPinterest, FaYoutube} from 'react-icons'
+import React, {useState} from 'react';
+import {BsPerson} from 'react-icons/bs';
+import {BiSearch} from 'react-icons/bi';
+import {AiOutlineClose} from 'react-icons/ai';
+import {HiOutlineMenuAlt4} from 'react-icons/hi';
+import {FaFacebook, FaTwitter, FaInstagram, FaPinterest, FaYoutube} from 'react-icons/fa';
 
 const Navbar = () => {
   return (
@@ -25,27 +25,33 @@ const Navbar = () => {
 <div className='md:hidden'>
     <HiOutlineMenuAlt4 size={20} />
 </div>
-<div>
+    {/* Mobile menu dropdown */}
+<div className='absolute left-[-100%] top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col'>
     <ul>
         <h1>BEACHSE.</h1>
-        <l1>Home</l1>
-        <l1>Destinations</l1>
-        <l1>Travel</l1>
-        <l1>View</l1>
-        <l1>Book</l1>
-        <div>
-            <button>Search</button>
-            <button>Accouny</button>
+        <l1 className='border-b'>Home</l1>
+        <l1 className='border-b'>Destinations</l1>
+        <l1 className='border-b'>Travel</l1>
+        <l1 className='border-b'>View</l1>
+        <l1 className='border-b'>Book</l1>
+        <div className='flex flex-col'>
+            <button className='my-6'>Search</button>
+            <button>Account</button>
         </div>
-        <div>
+        <div className='flex justify-between my-6'>
             <FaFacebooks className='icon' />
             <FaTwitter className='icon' />
             <FaYoutube className='icon' />
             <FaPinterest className='icon' />
             <FaInstagram className='icon' />
         </div>
+    
 
-    </ul>
+        </ul>
 </div>
+</div>
+  );
+  
+};
 
-export default Navbar
+export default Navbar;
